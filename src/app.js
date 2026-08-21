8226af6600538f932fdbd6094adf91d3b16c7c6d
AyushNagar52 <ayushnagar5350@gmail.com>
2026-08-21 12:41:23 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res) =>  {
+  res.send("Hello from the server!");
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server is successfully listenin on port 3000");
+});
