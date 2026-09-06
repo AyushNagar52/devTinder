27f60616c611167d8dbedd3551be340292f51109
AyushNagar52 <ayushnagar5350@gmail.com>
2026-09-06 22:21:32 +0530

    Enhance user model validation and update functionality; add gender validation and improve user update logic

--- src/app.js
+++ src/app.js
@@ -55,11 +55,23 @@ app.delete("/user", async (req, res) => {
     }
 });
 
+const ALLOWED_UPDATES = ["password", "photourl", "about", "gender", "skills"];   
+
+const isUpdateAllowed = Objectkeys(data).every((k) =>
+    ALLOWED_UPDATES.includes(k)
+);
+  
+
 // Update data of the user
 app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
     const data = req.body;
     try {
-        const user = await User.findByIdAndUpdate(data.userId, data, { new: true });
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+        });
+        console.log(user);
         res.send("User updated successfully");
     } catch (err) {
         res.status(400).send("Something went wrong:" + err.message);
--- src/models/user.js
+++ src/models/user.js
@@ -5,31 +5,59 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
  firstName: {
     type: String,
-    required: true
+    required: true,
+    minlength: 4
  },
  lastName: {
     type: String,
-    required: true
+    
  },
  emailId: {
     type: String,
-    required: true
+    lowercase: true,
+    required: true,
+    unique: true,
+    trim: true,
+    
  },
     password: {
         type: String,
         required: true
+        
     },
 
     age: {
         type: Number,
+        min: 18,
         required: true
     },
     gender: {
         type: String,
+        validate(value) {
+            if (!["male", "female", "other"].includes(value)) {
+                throw new Error("Gender data is not valid");
+            }
+        },
         required: true
     },
 
-});
+    photourl: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"},
+
+    about: {
+        type: String,
+        default: "This is a default about section.",
+    },
+
+    skills: {
+        type: [String],
+    },
+},
+{
+timestamps: true,
+}
+);
 
 const userModel = mongoose.model('User', userSchema);
 
