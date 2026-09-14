0beae53caf92c330de0a0536e5cf0236cd333d66
AyushNagar52 <ayushnagar5350@gmail.com>
2026-09-14 14:26:25 +0530

    Refactor profile routes and enhance validation for profile editing

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const profileRouter = express.Router();
+
 const { userAuth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
-   try{
-    const user = req.user;
 
-    res.send(user);
-    }  catch (err) {
+// View profile
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        res.send(user);
+    } catch (err) {
         res.status(400).send("Error: " + err.message);
-    }  
+    }
+});
+
+// Edit profile
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+       if (!validateEditProfileData(req)){
+        throw new Error("Invalid Edit Request");
+       }
+const loggenInUser = req.user;
+
+Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+await loggenInUser.save();
+
+    res.json({
+        message: '${loggedInUser.firstName}, your profile updated successfully'
+    });
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
 });
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,19 +1,54 @@
 const validator = require("validator");
 
-
-
 const validateSignupData = (req) => {
-
-    const {firstName, lastName, emailId, password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
 
     if (!firstName || !lastName) {
         throw new Error("First name and last name are required");
-    }
-    else if (!emailId || !validator.isEmail(emailId)) {
+    } else if (firstName.length < 4 || firstName.length > 50) {
+        throw new Error("First name must be between 4 and 50 characters");
+    } else if (!emailId || !validator.isEmail(emailId)) {
         throw new Error("Email is not valid");
-    }
-    else if (!password || !validator.isStrongPassword(password)) {
-        throw new Error("Enter a strong password");
+    } else if (!password || !validator.isStrongPassword(password)) {
+        throw new Error("Please enter a strong password (minimum 8 characters with at least 1 uppercase, 1 lowercase, 1 number, and 1 symbol)");
     }
 };
-module.exports = { validateSignupData };
+
+const validateEditProfileData = (req) => {
+    const allowedEditFields = [
+        "firstName",
+        "lastName",
+        "photourl",
+        "gender",
+        "age",
+        "about",
+        "skills",
+    ];
+
+    const isEditAllowed = Object.keys(req.body).length > 0 && 
+        Object.keys(req.body).every((field) => allowedEditFields.includes(field));
+
+    return isEditAllowed;
+};
+
+const validatePasswordUpdate = (req) => {
+    const { currentPassword, newPassword } = req.body;
+
+    if (!currentPassword || !newPassword) {
+        throw new Error("Current password and new password are required");
+    }
+
+    if (currentPassword === newPassword) {
+        throw new Error("New password cannot be the same as the current password");
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+        throw new Error("New password must be strong (minimum 8 characters with at least 1 uppercase, 1 lowercase, 1 number, and 1 symbol)");
+    }
+};
+
+module.exports = {
+    validateSignupData,
+    validateEditProfileData,
+    validatePasswordUpdate,
+};
