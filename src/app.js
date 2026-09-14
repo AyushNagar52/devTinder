bf65e9382cf7ce0632d83092c13df7c795bbbd51
AyushNagar52 <ayushnagar5350@gmail.com>
2026-09-14 11:41:08 +0530

    Implement user logout functionality and fix password validation in user model

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 
 // Get user by email 
